c5e303d1ee9d538f5c093e3d9ea9befb205fac25
Paul R <rudnikov.paul@yandex.ru>
2025-12-05 18:06:21 +0500

    Delete lessons/lesson 29 (04.12). Коллекции уровня «джун+». списки, срезы, вложенные структуры/tasks/1/PR29.py

--- lessons/lesson 29 (04.12). Коллекции уровня «джун+». списки, срезы, вложенные структуры/tasks/1/PR29.py
+++
@@ -1,8 +0,0 @@
-base_set = ["кисть круглая", "кисть плоская", "гуашь"]
-extras = ["акварель", "палитра"]
-base_set.extend(extras)
-print("Всего предметов: ", len(base_set))
-print("Второй предмет: ",(base_set[1]))
-print("Последний предмет: ",(base_set[-1]))
-for index, item in enumerate(base_set, start=1):
-    print(f"{index}. {item}")
