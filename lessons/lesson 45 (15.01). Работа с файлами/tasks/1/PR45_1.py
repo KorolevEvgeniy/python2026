27135fd6e80efa67c7dc2c9877c81c9b8f9f64f4
Королев Евгений <udjinnk@gmail.com>
2026-01-15 20:29:11 +0500

    Add files via upload

---
+++ lessons/lesson 45 (15.01). Работа с файлами/tasks/1/PR45_1.py
@@ -0,0 +1,18 @@
+def print_attendees(path: str):
+        try:
+        with open(path.txt, 'r', encoding='utf-8') as file:
+                    lines = file.readlines()
+        except FileNotFoundError:
+                print("Нет файла со списком участников")
+                return
+        attendees = []
+        for line in lines:
+                cleaned_name = line.strip()
+                if cleaned_name: 
+                    attendees.append(cleaned_name)
+        if not attendees:
+                print("Список участников пуст")
+                return        
+        print(f"Участники ({len(attendees)}):")
+        for i, name in enumerate(attendees, start=1):
+                print(f"{i}) {name}")
