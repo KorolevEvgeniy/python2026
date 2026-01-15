a19dd24a532e579ecb5c3daf04b5ddbee265b0dc
Королев Евгений <udjinnk@gmail.com>
2026-01-15 21:04:49 +0500

    Add files via upload

--- lessons/lesson 45 (15.01). Работа с файлами/tasks/1/PR45_1.py
+++ lessons/lesson 45 (15.01). Работа с файлами/tasks/1/PR45_1.py
@@ -1,6 +1,6 @@
 def print_attendees(path: str):
         try:
-        with open(path.txt, 'r', encoding='utf-8') as file:
+                with open(path.txt, 'r', encoding='utf-8') as file:
                     lines = file.readlines()
         except FileNotFoundError:
                 print("Нет файла со списком участников")
